6a4d704ae36a8738ab0f570efd8c7a436573292d
mmach900 <m.mach.90@gmail.com>
2020-04-14 15:57:45 +0200

    no message

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const CONN_URL = process.env.AMQP ? process.env.AMQP : 'amqp://oqxnmzzs:hUxy1BVE
 const PREFETCH = process.env.PREFETCH ? process.env.PREFETCH : 5;
 const MAX_CONNECTIONS = process.env.MAX_CONNECTIONS ? process.env.MAX_CONNECTIONS : 10;
 var queueSvc = azure.createQueueService();
+queueSvc.messageEncoder = new azure.QueueMessageEncoder.TextBase64QueueMessageEncoder();
 
 global.c = new Crawler({
     maxConnections: MAX_CONNECTIONS,
